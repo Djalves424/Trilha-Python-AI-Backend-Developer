de889d18794096cd3f4199ffdd5825aa20899945
Diego Alves <djalves424@gmail.com>
2024-05-09 21:04:08 -0300

    Created data structure classes in the conjuncts layer

---
+++ 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
@@ -0,0 +1,11 @@
+numeros = set([1, 2, 3, 1, 3, 4])
+print(numeros)  # {1, 2, 3, 4}
+
+letras = set("abacaxi")
+print(letras)  # {"b", "a", "c", "x", "i"}
+
+carros = set(("palio", "gol", "celta", "palio"))
+print(carros)  # {"gol", "celta", "palio"}
+
+linguagens = {"Python", "Java", "C#", "JavaScript", "HTML", "CSS", "Python"}
+print(linguagens)
